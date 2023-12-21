725dc6f998e18ec301b83ac9d3cbe86adc5ad231
Abdelrahaman Nader <abualiya.2202@gmail.com>
2023-12-21 18:48:11 +0200

    tailwind

--- EltaherGroub/tailwind.config.js
+++ EltaherGroub/tailwind.config.js
@@ -6,8 +6,15 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-      gradientcolor: 'linear-gradient(150deg, #5b4516,#b4903a,#fffea6,#b4903a,#b4903a,#b4903a,#fffea6,#b4903a)',
-      textcolor: '#001D3D',
+      // 'gradientcolor': 'linear-gradient(150deg, #5b4516,#b4903a,#fffea6,#b4903a,#b4903a,#b4903a,#fffea6,#b4903a)',
+      'textcolor': '#001D3D',
+      'maincolor' : '#FFD60A',
+      'secondcolor': '#FFC300',
+      'textcolor2':'#001D3D',
+      'lightecolor':'rgb(255, 255, 254)',
+      'mainsecondcolor':'#629bd9f7',
+      // 'gradientcolor':'linear-gradient(150deg, #5b4516,#b4903a,#fffea6,#b4903a,#b4903a,#b4903a,#fffea6,#b4903a)',
+
 
   }
     },
